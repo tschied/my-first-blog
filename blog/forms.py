c1195e5c8bca50636dbe94fd6880635641e82427
thomass <t1001@schied.net>
2022-05-23 23:17:31 +0200

    Add button hinzugefügt

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from .models import Post
+
 class PostForm(forms.ModelForm):
+
     class Meta:
-        model = Postfields = ('title','text')
+        model = Post
+        fields = ('title','text',)
